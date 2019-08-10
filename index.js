9b0d331e50f16ece4c8deab5e20c75441c7d6eb0
Alex Pariona <lx7pary@gmail.com>
2019-08-10 13:02:09 -0500

    add scraping

--- index.js
+++ index.js
@@ -23,22 +23,3 @@ app.listen(port, function(err){
     }
     console.info('>>> Open http://localhost:%s/ in your browser.', port);
   });
-
-
-/*
-…or create a new repository on the command line
-echo "# consultape" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/surenperu/consultape.git
-git push -u origin master
-
-…or push an existing repository from the command line
-git remote add origin https://github.com/surenperu/consultape.git
-git push -u origin master
-*/
-
-
-
-
